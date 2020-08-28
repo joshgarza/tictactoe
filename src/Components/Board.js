d1b463453f75551703aef3aab3b597561a0faf57
Josh <josh@sf-iron.com>
2020-08-27 20:38:57 -0700

    Add calculateWinner function

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,6 +1,28 @@
 import React, {Component} from 'react';
 import Square from './Square';
 
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        console.log(lines[i]);
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            // console.log(squares[b]);
+            return squares[a];
+        }
+        return null;
+    }
+}
+
 class Board extends React.Component {
     constructor(props) {
         super(props);
@@ -10,29 +32,10 @@ class Board extends React.Component {
         };
     }
 
-    calculateWinner(squares) {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }
-            return null;
-        }
-    }
-
     handleClick(i) {
         const squares = this.state.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
+            console.log("clicked");
             return;
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
@@ -55,6 +58,7 @@ class Board extends React.Component {
         const winner = calculateWinner(this.state.squares);
         let status;
         if (winner) {
+            console.log("winner");
             status = "Winner: " + winner;
         } else {
             status = "Next player: " + (this.state.xIsNext ? 'X' : 'O')
